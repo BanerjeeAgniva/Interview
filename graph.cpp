eb7d1e5af6a79e82fedf4b9b5f2acbc45ae75ea2
BanerjeeAgniva <f20200922@pilani.bits-pilani.ac.in>
2024-12-31 13:52:45 +0530

    flood fill

--- graph.cpp
+++ graph.cpp
@@ -100,3 +100,58 @@ Total_time days - total time taken to rotten.
 
   return tot == cnt ? days : -1;
 }
+/*
+FLOOD FILL
+You are given an image represented by an m x n grid of integers image, where
+image[i][j] represents the pixel value of the image. You are also given three
+integers sr, sc, and color. Your task is to perform a flood fill on the image
+starting from the pixel image[sr][sc].
+
+To perform a flood fill:
+
+Begin with the starting pixel and change its color to color.
+Perform the same process for each pixel that is directly adjacent (pixels that
+share a side with the original pixel, either horizontally or vertically) and
+shares the same color as the starting pixel. Keep repeating this process by
+checking neighboring pixels of the updated pixels and modifying their color if
+it matches the original color of the starting pixel. The process stops when
+there are no more adjacent pixels of the original color to update. Return the
+modified image after performing the flood fill.
+
+Example 1:
+
+Input: image = [[1,1,1],
+                [1,1,0],
+                [1,0,1]], sr = 1, sc = 1, color = 2
+
+Output:         [[2,2,2],
+                 [2,2,0],
+                 [2,0,1]]
+*/
+void dfs(int row, int col, vector<vector<int>> &ans, vector<vector<int>> &image,
+         int newColor, int delRow[], int delCol[], int iniColor) {
+  // color with new color
+  ans[row][col] = newColor;
+  int n = image.size();
+  int m = image[0].size();
+  // there are exactly 4 neighbours
+  for (int i = 0; i < 4; i++) {
+    int nrow = row + delRow[i];
+    int ncol = col + delCol[i];
+    // check for valid coordinate
+    // then check for same initial color and unvisited pixel
+    if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+        image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor) {
+      dfs(nrow, ncol, ans, image, newColor, delRow, delCol, iniColor);
+    }
+  }
+}
+vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc,
+                              int newColor) {
+  int iniColor = image[sr][sc];
+  vector<vector<int>> ans = image;
+  int delRow[] = {-1, 0, +1, 0};
+  int delCol[] = {0, +1, 0, -1};
+  dfs(sr, sc, ans, image, newColor, delRow, delCol, iniColor);
+  return ans;
+}
