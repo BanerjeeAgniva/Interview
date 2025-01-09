0325adb396ac2456eed094f9d634181668fdbebd
BanerjeeAgniva <f20200922@pilani.bits-pilani.ac.in>
2025-01-09 09:46:54 +0530

    dataviz

---
+++ tata1mg.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector> 
+
+using namespace std;
+int func(vector<int>& dp,vector<int>& v,int ind)
+{
+    int skip=ind+1;
+    int ans=INT_MAX;
+    ans=min//pick 
+}
+int main()
+{
+    int n; cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+    {
+       cin>>v[i];
+    }
+    vector<int> dp(n,-1);
+    dp[0]=func(dp,v,0);
+}
